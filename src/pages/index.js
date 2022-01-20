c1c7c6940ee86a327deba13ab320d205bf8ef6e2
unknown <telkom@G8153C5Q_940323.telkom.co.id>
2022-01-20 14:43:59 +0700

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,8 +9,9 @@ const IndexPage = () => {
     <Layout pageTitle="HomePage">
       <p>I'm making this by following the Gatsby Tutorial.</p>
       <StaticImage
-        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
-        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+
+        alt="Image From Local"
+        src="../images/icon.png"
       ></StaticImage>
     </Layout>
 
